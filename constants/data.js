58730149bb0397f0e1a38ad78da52e2affea7970
Sanjay Bisht <69279043+biishtsanjay@users.noreply.github.com>
2023-09-25 01:33:57 +0530

    initial committ

---
+++ constants/data.js
@@ -0,0 +1,90 @@
+const userData = {
+  githubUsername: "biishtsanjay",
+  name: "Sanjay Bisht",
+  designation: "Developer",
+  avatarUrl: "/avatar.jpg",
+  email: "sanjayb0407@gmail.com",
+  phone: "+91 9116484395",
+  address: "Bengaluru, Karnataka, India.",
+  projects: [
+    // {
+    //   // title: "Test",
+    //   // link: "https://test.com",
+    //   // imgUrl: "/test.jpg",
+    // },
+    // {
+    //   // title: "Test",
+    //   // link: "https://test.com",
+    //   // imgUrl: "/test.jpg",
+    // },
+  ],
+  about: {
+    title:
+      "I'm a software developer that loves building products and web applications that impact millions of lives",
+    description: [
+      `HELLO, I'm a passionate frontend developer with two years of experience in crafting web applications. My journey into the world of web development has been an exhilarating ride, where I've had the privilege to bring digital ideas to life.`,
+      `As a frontend developer, I specialize in translating creative designs into interactive and user-friendly websites. My skills encompass HTML, CSS, JavaScript, React, Storybook, Jest, RTL, etc., and I take pride in ensuring that every project I work on is responsive, cross-browser compatible, and optimized for peak performance.`,
+      `Beyond my love for coding, I'm an avid music enthusiast. Music is my muse outside the world of development. Whether I'm singing, strumming my guitar, exploring music production, or simply enjoying various genres, it's my source of inspiration and relaxation`,
+      `What drives me is a relentless curiosity for technology. In this ever-evolving field, I thrive on learning new things. From attending tech conferences to diving into online courses, I'm committed to staying at the forefront of the industry.`,
+      `I believe in the power of innovation and creativity to transform the digital landscape. If you share my enthusiasm for web development, music, or tech exploration, let's connect and explore the exciting possibilities that lie ahead. Welcome to my portfolio, where code meets creativity.`,
+    ],
+    // currentProject: "Tailwind Master Kit",
+    // currentProjectUrl: "https://tailwindmasterkit.com",
+  },
+  experience: [
+    {
+      title: "Frontend Developer",
+      company: "Locad",
+      year: "2022 - Present",
+      companyLink: "https://golocad.com/",
+      // desc: "Contributed to our flagship product - Panna - which is an AI powered candidate interviewing product.",
+      desc: [
+        `Frontend Developer at Locad for two years, proficient in building user-centric web applications.`,
+        ` Expertise in performance optimization, accessibility, and contemporary design principles for exceptional digital experiences.`,
+      ],
+      // "Frontend developer at LOCAD, crafting seamless web experiences, pixel by pixel, for the past two years. Code magician at your service!",
+    },
+    {
+      title: "Internship",
+      company: "Open Sense Labs, (Remote)",
+      year: "2021",
+      companyLink: "https://opensenselabs.com/",
+      // desc: "Developed pothole detection system with OpenCV and computer vision, detects potholes on roads and informs the driver.",
+      desc: "Change Here",
+    },
+    {
+      title: "Graduation",
+      company: "Bangalore Institute of Technology, Bengaluru (KA).",
+      year: "2021",
+      companyLink: "https://bit-bangalore.edu.in/",
+      // desc: "Major in Mechanical Engineering with a CGPA of 7.5. Nobody asks this but it's okay.",
+    },
+    {
+      title: "High School",
+      company: "Beersheba Sr. Sec. Institute, Haldwani (Nainital).",
+      year: "2016",
+      companyLink: "http://www.beershebaschool.in/school/Haldwani",
+      // desc: "Subjects being PCM - I barely survived with 75% marks aggregate.",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1lUm-qgxfaLmTggr12j1X4Sny-ea4O-mg/view?usp=sharing",
+  socialLinks: {
+    instagram: "https://www.instagram.com/biishtplease/",
+    twitter: "https://twitter.com/biishtsanjay",
+    linkedin: "https://www.linkedin.com/in/sanjaybiisht/",
+    github: "https://github.com/biishtsanjay",
+  },
+};
+
+export default userData;
+
+export const techStack = [
+  "Reactjs",
+  "Javascript",
+  "Material UI",
+  "TailwindCSS",
+  "Storybook",
+  "Jest",
+  "Playwright",
+];
