26cba03299ff6ffc1828e5411a7fcbcc5dcd0c28
biishtsanjay <sanjayb0407@gmail.com>
2023-10-17 01:30:15 +0530

    'WIP'

--- components/Projects.js
+++ components/Projects.js
@@ -13,7 +13,7 @@ export default function Projects() {
       <div className="bg-[#F1F1F1] dark:bg-gray-900">
         {/* <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 py-20 pb-40"> */}
         <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 py-20 pb-40 text-2xl">
-          <p>Will be back with awesome projects ... 🧨 🚀</p>
+          <p>Will be back with awesome projects... 🧨 🚀</p>
 
           {/* {userData.projects.map((proj, idx) => (
             <ProjectCard
